a2e02a1a545c1e260b301c5af9320e5d5e702e70
student5 <studkab5@jdk.ru>
2024-03-06 14:50:21 +0500

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Post extends Model
+{
+    use Sluggable;
+    public function tags()
+    {
+        return $this-> belongsToMany(Tag::class);
+    }
+    public function category()
+    {
+        return $this-> belongTo(Category::class);
+    }
+
+
+    public function sluggable()
+    {
+        return [
+            'slug'=> [
+                'source'=>'title'
+            ]
+            ];
+    }
+}
